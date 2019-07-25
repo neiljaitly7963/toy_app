ce71e14efd130028323a3af818aeedadd5b2a464
neiljaitly7963 <neiljaitly7963@gmail.com>
2019-07-25 19:11:36 +0530

    Finish Toy app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_07_25_125813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,23 @@ ActiveRecord::Schema.define(version: 0) do
     t.string "order_title", limit: 50, null: false
   end
 
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "orders", id: :serial, force: :cascade do |t|
     t.string "order_title", limit: 50, null: false
     t.integer "price", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
